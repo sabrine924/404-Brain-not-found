72f187e95ebe393f1c082636f44f2eb9e024a4da
YoussefBA <YoussefBA@DESKTOP-CI9UU49>
2021-03-11 23:42:18 +0100

    first commit.

---
+++ SpringProjectGroup/src/main/java/com/esprit/spring/SpringProjectGroupApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectGroupApplication.class, args);
+	}
+
+}
